6c7389aee9646886d6560ce0c3cfffd95817b97e
Tran Tam <tranngoctam@Tam-iMac.local>
2017-11-02 15:39:25 +0700

    Initial Commit

---
+++ CaBuGmap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaBuGmap
+//
+//  Created by Tran Ngoc Tam on 11/2/17.
+//  Copyright © 2017 Tran Ngoc Tam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
